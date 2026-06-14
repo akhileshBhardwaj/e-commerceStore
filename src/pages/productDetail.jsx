394f481bda568afd650db1d810323f30b20687b6
akhileshBhardwaj <rajbharakhilesh818@gmail.com>
2026-06-14 08:12:43 +0530

    updated

--- src/pages/productDetail.jsx
+++ src/pages/productDetail.jsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { initialProducts } from "../data/Product";
+import { Link } from "lucide-react";
+import { ChevronLeft } from "lucide-react";
 
 const ProductDetail = () => {
-  console.log("This is a product detailes page", useParams());
-  return <div>productDetail</div>;
+  const { id } = useParams();
+
+  const [product, setProduct] = useState();
+
+  useEffect(() => {
+    setProduct(initialProducts.find((data) => data.id == id));
+  }, [id]);
+  console.log("myPrduct:", product);
+
+  return (
+    <>
+      <div className="container">
+        <Link to={"/"}>
+          <button>
+            {" "}
+            <ChevronLeft />{" "}
+          </button>
+        </Link>
+      </div>
+    </>
+  );
 };
 
 export default ProductDetail;
